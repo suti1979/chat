ea262d618aeee44116654382f0c4d2af7161529c
suti1979 <suti1979@gmail.com>
2022-03-31 10:28:21 +0200

    styling

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,24 +1,24 @@
-import Messages from "./Messages";
+import Messages from "./Messages"
 
 export default function App() {
-
   return (
     <div className="App">
-      <header className="App-header">
+      <main>
         <Messages />
-        <div className="container fixed-bottom pt-2" id="newmessage">
-          <form>
-            <div className="input-group">
-              <textarea className="form-control" id="message" aria-label="With textarea" />
-              <input className="btn btn-outline-light" type="submit" value="&#8688;" />
-            </div>
+      </main>
 
-            <div className="input-group input-group-sm mt-1">
-              <input className="form-control" type="file" id="formFile" />
-            </div>
-          </form>
-        </div>
-      </header>
+      <footer id="newmessage">
+        <form>
+          <div className="input-group">
+            <textarea className="form-control" id="message-textarea" aria-label="With textarea" />
+            <input className="btn btn-primary" type="submit" value="&#8688;" />
+          </div>
+
+          <div className="input-group input-group-sm mt-1">
+            <input className="form-control" type="file" id="formFile" />
+          </div>
+        </form>
+      </footer>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Container } from "react-bootstrap"
 import "./assets/custom.scss"
-import "./assets/style.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Container>
       <App />
-    </Container>
   </React.StrictMode>,
   document.getElementById("root")
 )
