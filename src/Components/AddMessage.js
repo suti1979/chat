627edcf433a81384ad6f43dcc1cf7d0cc9f4ad99
suti1979 <suti1979@gmail.com>
2022-04-01 13:37:58 +0200

    add scroll down ref

--- src/Components/AddMessage.js
+++ src/Components/AddMessage.js
@@ -7,32 +7,30 @@ export default function AddMessage() {
   const { handleMessageAdd } = useContext(MessagesCtx)
   const [newMessage, setNewMessage] = useState([])
 
-  useEffect(()=>{
-      setNewMessage({userName: `${DUMMY_USERNAME}`})
-  },[])
+  useEffect(() => {
+    setNewMessage({ userName: `${DUMMY_USERNAME}` })
+  }, [])
 
   function handleChange(value) {
-      setNewMessage({...newMessage, ...value})
+    setNewMessage({ ...newMessage, ...value })
   }
 
   return (
     <footer>
-   
-        <label htmlFor="message-textarea" className="form-label">
-          {newMessage.userName}
-        </label>
-        <input
-          type="text"
-          value={newMessage.message || ""}
-          onChange={(e) => handleChange({ message: e.target.value })}
-          name="textmessage"
-          className="form-control m-2"
-          id="message-textarea"
-        />
-        <button onClick={() => handleMessageAdd(newMessage)} className="btn btn-primary">
+      <label htmlFor="message-textarea" className="form-label">
+        {newMessage.userName}
+      </label>
+      <input
+        type="text"
+        value={newMessage.message || ""}
+        onChange={(e) => handleChange({ message: e.target.value })}
+        name="textmessage"
+        className="form-control m-2"
+        id="message-textarea"
+      />
+      <button onClick={() => handleMessageAdd(newMessage)} className="btn btn-primary">
         &#8688;
       </button>
-  
     </footer>
   )
 }
