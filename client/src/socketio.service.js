d5372fcfee08a3d40a55bb05149f854bd310e279
suti1979 <suti1979@gmail.com>
2022-04-26 10:45:53 +0200

    some changes

--- client/src/socketio.service.js
+++ client/src/socketio.service.js
@@ -3,7 +3,8 @@ import { io } from "socket.io-client"
 export let socket
 
 export const initiateSocketConnection = () => {
-  socket = io(process.env.REACT_APP_SOCKET_ENDPOINT)
+  console.log(process.env.REACT_APP_SOCKET_ENDPOINT)
+  socket = io("http://localhost:4001/")
 }
 
 export const sendMsg = (msg) => {
